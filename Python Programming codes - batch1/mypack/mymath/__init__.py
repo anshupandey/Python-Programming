715c2c141261d953990d60830a37b94f4e5004e3
Anshu Pandey <anshuspandey@gmail.com>
2021-06-18 09:05:56 +0530

    modules and package codes for batch1

---
+++ Python Programming codes - batch1/mypack/mymath/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 12:11:44 2021
+
+@author: Nitu
+"""
---
+++ Python Programming codes - batch1/mypack/mymath/urmath.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 12:12:22 2021
+
+@author: Nitu
+"""
+
+
+
+def addtwo(a,b):
+    return a+b
+
+def multtwo(a,b):
+    return a*b
