363aba3fcb865657f14f78a2e115be4fd1580446
Anshu Pandey <anshuspandey@gmail.com>
2021-11-13 01:21:25 +0530

    python package creation

---
+++ Xceedance/mypack/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 11:06:39 2021
+
+@author: anshu
+"""
---
+++ Xceedance/mypack/oct.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 11 11:06:39 2021
+
+@author: anshu
+"""
+
+def octfun(a,b):
+    return a+b
+
+
+class OA:
+    x = 5
+    y = 6
+    def func1(self,a,*b):
+        print("I am a function of class A")
+        return a + sum(b)
+    
+    def func2(self):
+        print("I am func2 , a function of class A")
+        
+        
