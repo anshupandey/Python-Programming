602e0a57f07d51ef94b378d6725823d4dc6739fb
Anshu Pandey <anshuspandey@gmail.com>
2021-12-25 23:39:14 +0530

    UDP server cde

---
+++ Capgemini/Flask Course/code_python_UDP_server.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 23 15:35:05 2021
+
+@author: anshu
+"""
+
+# UDP Server
+
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+host = "127.0.0.1"
+port = 12346
+
+s.bind((host,port))
+
+while True:
+    data,addr = s.recvfrom(2048)
+    print(data)
+    s.sendto(b"Hey hi client, thanks for your data",addr)
